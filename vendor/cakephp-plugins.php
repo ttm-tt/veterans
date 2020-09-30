d40abda066c6ac4b317531d148b8d6f6e1187357
christoph <christoph@7754d8ab-9b54-e011-a251-0001c006bdc7>
2020-09-30 12:57:58 +0000

    Update vendor plugins
    git-svn-id: https://localhost/svn/veterans/trunk@7122 7754d8ab-9b54-e011-a251-0001c006bdc7

--- vendor/cakephp-plugins.php
+++ vendor/cakephp-plugins.php
@@ -3,12 +3,12 @@ $baseDir = dirname(dirname(__FILE__));
 return [
     'plugins' => [
         'ErrorEmail' => $baseDir . '/vendor/ebrigham1/cakephp-error-email/',
-        'Shim' => $baseDir . '/vendor/dereuromark/cakephp-shim/',
         'Cake/TwigView' => $baseDir . '/vendor/cakephp/twig-view/',
         'Bake' => $baseDir . '/vendor/cakephp/bake/',
         'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
         'Acl' => $baseDir . '/vendor/cakephp/acl/',
         'CakePdf' => $baseDir . '/vendor/friendsofcake/cakepdf/',
-        'Migrations' => $baseDir . '/vendor/cakephp/migrations/'
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
+        'Shim' => $baseDir . '/vendor/dereuromark/cakephp-shim/'
     ]
 ];
