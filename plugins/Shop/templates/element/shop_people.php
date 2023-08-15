d6591fa3133b1003e24689e296b5e2a77c70b290
christoph <christoph@7754d8ab-9b54-e011-a251-0001c006bdc7>
2023-08-15 16:58:06 +0000

    Add instructions for players to be classified for para events
    git-svn-id: https://localhost/svn/veterans/trunk@8712 7754d8ab-9b54-e011-a251-0001c006bdc7

--- plugins/Shop/templates/element/shop_people.php
+++ plugins/Shop/templates/element/shop_people.php
@@ -13,16 +13,23 @@
 <?php
 	$players = array();
 	$accs = array();
+	$wantClassification = array();
 
 	foreach ($people as $person) {
 		if (!empty($person['type'])) {
-			if ($person['type'] === 'PLA')
+			if ($person['type'] === 'PLA') {
 				$players[] = $person;
+				if (($person['ptt_class'] ?? 0) < 0)
+					$wantClassification[] = $person;
+			}
 			if ($person['type'] === 'ACC')
 				$accs[] = $person;
 		} else {
-			if ($types[$person['type_id']] === 'PLA')
+			if ($types[$person['type_id']] === 'PLA') {
 				$players[] = $person;
+				if (($person['ptt_class'] ?? 0) < 0)
+					$wantClassification[] = $person;
+			}
 			if ($types[$person['type_id']] === 'ACC')
 				$accs[] = $person;
 		}
@@ -61,6 +68,7 @@
 				<th><?php echo __d('user', 'Association');?></th>
 				<th><?php echo __d('user', 'Sex');?></th>
 				<th><?php echo __d('user', 'Born');?></th>
+				<th><?php echo __d('user', 'Para'); ?></th>
 				<?php if ($events['S']) echo '<th>' . __d('user', 'Singles') . '</th>';?>
 				<?php if ($events['D']) echo '<th>' . __d('user', 'Doubles') . '</th>';?>
 				<?php if ($events['X']) echo '<th>' . __d('user', 'Mixed') . '</th>';?>
@@ -91,6 +99,12 @@
 					$year = substr ($dob, 0, 4);
 				else
 					$year = date('Y', strtotime($person['dob']));
+				
+				$ptt_class = $person['ptt_class'] ?? 0;
+				if ($ptt_class < 0)
+					$ptt_class = 't.b.c.';
+				else if ($ptt_class == 0)
+					$ptt_class = '';
 
 				echo '<tr ' . $class . '>';
 					echo '<td ' . $cancelled . '>' . $idx . '</td>';
@@ -99,6 +113,7 @@
 					echo '<td ' . $cancelled . '>' . $nations[$person['nation_id']] . '</td>';
 					echo '<td ' . $cancelled . '>' . $person['sex'] . '</td>';
 					echo '<td ' . $cancelled . '>' . $year . '</td>';
+					echo '<td ' . $cancelled . '>' . $ptt_class . '</td>';
 					if ($events['S']) echo '<td ' . $cancelled . '>' . $competitions[$person['single_id']]['name'] . '</td>';
 					if ($events['D']) echo '<td ' . $cancelled . '>' . $competitions[$person['double_id']]['name'] . '</td>';
 					if ($events['X']) echo '<td ' . $cancelled . '>' . $competitions[$person['mixed_id']]['name'] . '</td>';
@@ -110,6 +125,22 @@
 		?>
 		</tbody>
 	</table>
+	<?php 
+		if (count($wantClassification) > 0) {
+			echo '<br>';
+			echo __d('user', 'Players with para class \'t,b,c,\' (to be classified) need to undergo classification before they can participate in para events.');
+			echo __d('user', 'Classification will take place on the free day Wednesday, July 10th 2024.');
+			echo __d('user', 'To prepare follow the steps below:');
+			echo '<ul>';
+			echo '<li>';
+			echo __d('user', 'Please send their passports for registration using this form: {0}', 'https://forms.worldtabletennis.com/form/para_table_tennis_new_player_registration_en');
+			echo '</li>';
+			echo '<li>';
+			echo __d('user', 'Please send the medical documentation using this form: {0}', 'https://forms.worldtabletennis.com/form/para_table_tennis_new_player_registration_en');
+			echo '</li>';
+			echo '</ul>';
+		} 
+	?>
 	<br>
 
 <?php
