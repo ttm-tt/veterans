7dc1b476111d00be796f0e9e0b274bd8961e226e
christoph <christoph@7754d8ab-9b54-e011-a251-0001c006bdc7>
2023-10-15 18:36:56 +0000

    Use properties instead of array access
    git-svn-id: https://localhost/svn/veterans/trunk@8779 7754d8ab-9b54-e011-a251-0001c006bdc7

--- templates/Nations/view.php
+++ templates/Nations/view.php
@@ -4,32 +4,32 @@
 	<dl><?php $i = 0; $class = ' class="altrow"';?>
 		<dt<?php if ($i % 2 == 0) echo $class;?>><?php echo __('Name'); ?></dt>
 		<dd<?php if ($i++ % 2 == 0) echo $class;?>>
-			<?php echo $nation['name']; ?>
+			<?php echo $nation->name; ?>
 			&nbsp;
 		</dd>
 		<dt<?php if ($i % 2 == 0) echo $class;?>><?php echo __('Description'); ?></dt>
 		<dd<?php if ($i++ % 2 == 0) echo $class;?>>
-			<?php echo $nation['description']; ?>
+			<?php echo $nation->description; ?>
 			&nbsp;
 		</dd>
 		<dt<?php if ($i % 2 == 0) echo $class;?>><?php echo __('Continent'); ?></dt>
 		<dd<?php if ($i++ % 2 == 0) echo $class;?>>
-			<?php echo $nation['continent'] ?: '&nbsp;'; ?>
+			<?php echo $nation->continent ?: '&nbsp;'; ?>
 			&nbsp;
 		</dd>
 		<dt<?php if ($i % 2 == 0) echo $class;?>><?php echo __('Enabled'); ?></dt>
 		<dd<?php if ($i++ % 2 == 0) echo $class;?>>
-			<?php echo $nation['enabled']; ?>
+			<?php echo $nation->enabled; ?>
 			&nbsp;
 		</dd>
 		<dt<?php if ($i % 2 == 0) echo $class;?>><?php echo __('Updated At'); ?></dt>
 		<dd<?php if ($i++ % 2 == 0) echo $class;?>>
-			<?php echo $nation['modified']; ?>
+			<?php echo $nation->modified; ?>
 			&nbsp;
 		</dd>
 		<dt<?php if ($i % 2 == 0) echo $class;?>><?php echo __('Created At'); ?></dt>
 		<dd<?php if ($i++ % 2 == 0) echo $class;?>>
-			<?php echo $nation['created']; ?>
+			<?php echo $nation->created; ?>
 			&nbsp;
 		</dd>
 	</dl>
@@ -37,8 +37,8 @@
 
 <?php $this->start('action'); ?>
 	<ul>
-		<li><?php echo $this->Html->link(__('Edit Association'), array('action' => 'edit', $nation['id'])); ?> </li>
-		<li><?php echo $this->Html->link(__('Delete Association'), array('action' => 'delete', $nation['id']), ['confirm' => sprintf(__('Are you sure you want to delete %s?'), $nation['description'])]); ?> </li>
+		<li><?php echo $this->Html->link(__('Edit Association'), array('action' => 'edit', $nation->id)); ?> </li>
+		<li><?php echo $this->Html->link(__('Delete Association'), array('action' => 'delete', $nation->id), ['confirm' => sprintf(__('Are you sure you want to delete %s?'), $nation->description)]); ?> </li>
 		<li><?php echo $this->Html->link(__('List Associations'), array('action' => 'index')); ?> </li>
 		<li><?php echo $this->Html->link(__('New Association'), array('action' => 'add')); ?> </li>
 	</ul>
